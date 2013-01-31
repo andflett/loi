17eada25fdff72b927f8d9c52627e4611b00fd09
Andrew Sprinz <andrewsprinz@gmail.com>
2013-01-31 16:47:40 +0000

    Removing dead chapters

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,7 @@
 class AdminMailer < ActionMailer::Base
   
   default from: "LOI Web <noreply@leagueofintrapreneurs.com>",
-          to: (Rails.env.production?) ? Chapter.find_by_title('London').users.crew.map(&:email) : "andrewsprinz@gmail.com"
+          to: "andrewsprinz@gmail.com"
   
   def new_user(user)
     @user = user
